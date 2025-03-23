889b1ce116e76aa2b17196732bf272901c51083b
johnvo402 <jvanime212@gmail.com>
2025-03-23 17:21:11 +0000

    fix migation

---
+++ src/EcommeceService/Infrastructure/Data/Migrations/20250323171609_20250323171532_Project_Migration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250323171532_Project_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "payment_method",
+                table: "order_payment",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "payment_method",
+                table: "fund",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "payment_method",
+                table: "order_payment",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "payment_method",
+                table: "fund",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+        }
+    }
+}
