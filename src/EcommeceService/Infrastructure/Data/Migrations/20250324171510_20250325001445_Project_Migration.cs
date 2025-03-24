83ab78f6570bc97521a79f78aa5f0bbd998e90ae
TruongHoaiAnh <truonghoaianh0109@gmail.com>
2025-03-25 00:16:34 +0700

    Add file migration

---
+++ src/EcommeceService/Infrastructure/Data/Migrations/20250324171510_20250325001445_Project_Migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250325001445_Project_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
