8910e2ed02dffd34519cf7de0a9baa4e7b7b8887
truongnguyen <truongnn021203@gmail.com>
2025-03-26 01:27:28 +0700

    add filter order list

--- src/EcommeceService/Application/Feature/Orders/Queries/List/ListOrderQuery.cs
+++ src/EcommeceService/Application/Feature/Orders/Queries/List/ListOrderQuery.cs
@@ -1,9 +1,16 @@
 ﻿using JohnChum.SharedKernel.SpecificationQuery.LHS.Dtos.Requests;
 using JohnChum.SharedKernel.SpecificationQuery.LHS.Dtos.Responses;
 using Mediator;
-
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Feature.Orders.Queries.List
 {
-	public class ListOrderQuery : QueryParamRequest, IRequest<PaginationResponse<ListOrderResponse>>;
+    public class ListOrderQuery : QueryParamRequest, IRequest<PaginationResponse<ListOrderResponse>>
+    {
+        [FromQuery]
+        public string From { get; set; }
+
+        [FromQuery]
+        public string To { get; set; }
+    }
 }
