13bb0c7a3e73cd6d7ea83503ae1bd63e736b81ab
John Vo <jvanime212@gmail.com>
2025-06-27 01:16:38 +0700

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,26 +7,27 @@ cd $ROOT
 if [ -f "$ROOT/.env" ]; then
   export $(grep -v '^#' "$ROOT/.env" | xargs)
 fi
+
 # Kiểm tra tham số
-if [ -z "$1" ]; then
-  echo "Vui lòng truyền vào tên service. Ví dụ: ./deploy.sh user-service"
+if [ $# -eq 0 ]; then
+  echo "Vui lòng truyền vào ít nhất 1 service. Ví dụ: ./deploy.sh user-service"
   exit 1
 fi
 
-SERVICE=$1
+# Convert list of services to a string
+SERVICES="$@"
 
-ssh -p ${REMOTE_SERVER_PORT} ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP} << EOF
+ssh -p ${REMOTE_SERVER_PORT} ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP} bash << EOF
   echo "Vào thư mục micro..."
   cd ~/micro || exit 1
 
   echo "Git pull..."
   git pull || exit 1
 
-  echo "Make publish NAME=${SERVICE}..."
-  make publish NAME="${SERVICE}" || exit 1
-
-  echo "Make staging SERVICE=${SERVICE}..."
-  make staging SERVICE="${SERVICE}" || exit 1
-
-  echo "Triển khai hoàn tất cho service: ${SERVICE}"
+    echo "Make publish NAME=${SERVICES}..."
+    make publish NAME="${SERVICES}" || exit 1
+  
+    echo "Make staging SERVICE=${SERVICES}..."
+    make staging SERVICE="${SERVICES}" || exit 1
+    echo "Triển khai hoàn tất cho services: ${SERVICES}"
 EOF
