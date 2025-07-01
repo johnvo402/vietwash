193baaabd7965a12d3d12f5f1fd4eb008da2b24b
John Vo <jvanime212@gmail.com>
2025-07-02 01:30:19 +0700

    migration base entity

---
+++ src/EcommeceService/Infrastructure/Data/Migrations/20250701182439_20250702012426_Ecommerce_Migration.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250702012426_Ecommerce_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "voucher_customer",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "status",
+                table: "voucher",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "status",
+                table: "user",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "unit",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "service_tariff",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "service_price_tariff_history",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "id",
+                table: "repair_history",
+                type: "numeric(20,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "product_supplying",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "order_payment",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "order_item",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "inventory_relation",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "equipment_supplying",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "category",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "public_id",
+                table: "branch_user",
+                type: "character varying(26)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "voucher_customer");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "unit");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "service_tariff");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "service_price_tariff_history");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "product_supplying");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "order_payment");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "order_item");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "inventory_relation");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "equipment_supplying");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "category");
+
+            migrationBuilder.DropColumn(
+                name: "public_id",
+                table: "branch_user");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "status",
+                table: "voucher",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "status",
+                table: "user",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "id",
+                table: "repair_history",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)");
+        }
+    }
+}
