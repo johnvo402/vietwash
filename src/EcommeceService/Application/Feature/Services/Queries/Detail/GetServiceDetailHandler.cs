ee8140da94a77028fc1527b3a08f94147d5f9de6
TruongHoaiAnh <truonghoaianh0109@gmail.com>
2025-07-10 21:20:12 +0700

    add filed average rating into service detail

--- src/EcommeceService/Application/Feature/Services/Queries/Detail/GetServiceDetailHandler.cs
+++ src/EcommeceService/Application/Feature/Services/Queries/Detail/GetServiceDetailHandler.cs
@@ -3,11 +3,13 @@ using Application.Common.Interfaces.UnitOfWorks;
 using Application.Features.Common.Mapping.Users;
 using Contracts.ApiWrapper;
 using Contracts.Common.Messages;
+using Domain.Aggregates.Feedbacks;
 using Domain.Aggregates.Services;
 using Domain.Aggregates.Services.Specifications;
 using Domain.Aggregates.Users;
 using Domain.Aggregates.Users.Specifications;
 using Mediator;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Feature.Services.Queries.Detail;
 
@@ -60,6 +62,14 @@ public class GetServiceDetailHandler(IUnitOfWork unitOfWork)
             service.UpdatedUser = updatedUser;
         }
 
-        return Result<GetServiceDetailResponse>.Success(service);
+		var averageRating = await unitOfWork
+	        .Repository<Feedback>()
+	        .QueryAsync()
+	        .Where(x => x.ServiceId == service.Id && !x.Disable && x.Rating != null)
+	        .AverageAsync(x => (double?)x.Rating, cancellationToken) ?? 0;
+
+        service.AverageRating = averageRating;
+
+		return Result<GetServiceDetailResponse>.Success(service);
     }
 }
