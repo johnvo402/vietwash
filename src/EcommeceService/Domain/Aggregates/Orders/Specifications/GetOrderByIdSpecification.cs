1d0575abbdf59fe3952a89b55d4dae2d8bb0dcc9
John Vo <jvanime212@gmail.com>
2025-07-22 19:18:08 +0700

    fix detail order

--- src/EcommeceService/Domain/Aggregates/Orders/Specifications/GetOrderByIdSpecification.cs
+++ src/EcommeceService/Domain/Aggregates/Orders/Specifications/GetOrderByIdSpecification.cs
@@ -9,7 +9,7 @@ namespace Domain.Aggregates.Orders.Specifications
         public GetOrderByIdSpecification(long id)
         {
             Query
-                .Where(x => x.Id == id && x.Status == OrderStatus.Completed)
+                .Where(x => x.Id == id)
                 .Include(x => x.OrderItems)
                 .ThenInclude(x => x.Service)
                 .Include(x => x.Customer)
