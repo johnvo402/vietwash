52f466c4d860220d39a697cfe8eff74c138ec693
John Vo <jvanime212@gmail.com>
2025-07-23 19:23:07 +0700

    fix validate barcode

--- src/EcommeceService/Application/Feature/Common/Validators/Vouchers/VoucherValidator.cs
+++ src/EcommeceService/Application/Feature/Common/Validators/Vouchers/VoucherValidator.cs
@@ -9,8 +9,7 @@ using FluentValidation;
 
 namespace Application.Feature.Common.Validators.Vouchers
 {
-    public class VoucherValidator
-        : AbstractValidator<VoucherModel>
+    public class VoucherValidator : AbstractValidator<VoucherModel>
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly IActionAccessorService accessorService;
@@ -24,55 +23,49 @@ namespace Application.Feature.Common.Validators.Vouchers
 
         private void ApplyRules()
         {
-
-
             RuleFor(x => x.Title)
                 .NotEmpty()
-                .WithState(x => Messager
-                    .Create<Voucher>()
-                    .Property(x => x.Title)
-                    .Message(MessageType.Null)
-                    .Negative()
-                    .Build());
-
-            RuleFor(x => x.Barcode)
-                .NotEmpty()
-                .WithState(x => Messager
-                    .Create<Voucher>()
-                    .Property(x => x.Barcode)
-                    .Message(MessageType.Null)
-                    .Negative()
-                    .Build());
+                .WithState(x =>
+                    Messager
+                        .Create<Voucher>()
+                        .Property(x => x.Title)
+                        .Message(MessageType.Null)
+                        .Negative()
+                        .Build()
+                );
 
             RuleFor(x => x.ImgUrl)
                 .NotEmpty()
-                .WithState(x => Messager
-                    .Create<Voucher>()
-                    .Property(x => x.ImgUrl)
-                    .Message(MessageType.Null)
-                    .Negative()
-                    .Build());
+                .WithState(x =>
+                    Messager
+                        .Create<Voucher>()
+                        .Property(x => x.ImgUrl)
+                        .Message(MessageType.Null)
+                        .Negative()
+                        .Build()
+                );
 
             RuleFor(x => x.DiscountValue)
                 .GreaterThan(0)
-                .WithState(x => Messager
-                    .Create<Voucher>()
-                    .Property(x => x.DiscountValue)
-                    .Message(MessageType.LessThanEqual)
-                    .Negative()
-                    .Build());
+                .WithState(x =>
+                    Messager
+                        .Create<Voucher>()
+                        .Property(x => x.DiscountValue)
+                        .Message(MessageType.LessThanEqual)
+                        .Negative()
+                        .Build()
+                );
 
             RuleFor(x => x.EndAt)
                 .GreaterThan(x => x.StartAt)
-                .WithState(x => Messager
-                    .Create<Voucher>()
-                    .Property(x => x.EndAt)
-                    .Message(MessageType.Expired)
-                    .Negative()
-                    .Build());
-
+                .WithState(x =>
+                    Messager
+                        .Create<Voucher>()
+                        .Property(x => x.EndAt)
+                        .Message(MessageType.Expired)
+                        .Negative()
+                        .Build()
+                );
         }
-
-
     }
 }
