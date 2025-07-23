33414a09f1b88baf3c80abbb2c32a689ae008d28
John Vo <jvanime212@gmail.com>
2025-07-23 20:24:22 +0700

    fix update customer

--- src/AuthService/Application/Features/Customers/Command/Update/UpdateCustomerHandler.cs
+++ src/AuthService/Application/Features/Customers/Command/Update/UpdateCustomerHandler.cs
@@ -1,67 +1,66 @@
-﻿using Application.Common.Errors;
+﻿using System.Data.Common;
+using Application.Common.Errors;
 using Application.Common.Interfaces.Services.Identity;
 using Application.Common.Interfaces.UnitOfWorks;
 using Application.Features.Accounts.Commands.Update;
-using Contracts.Common.Messages;
-using Domain.Aggregates.Accounts.Specifications;
-using Domain.Aggregates.Accounts;
-using Mediator;
-using System.Data.Common;
 using Contracts.ApiWrapper;
+using Contracts.Common.Messages;
+using Domain.Aggregates.Accounts;
+using Domain.Aggregates.Accounts.Specifications;
+using Mediator;
 
 namespace Application.Features.Customers.Command.Update
 {
-	public class UpdateCustomerHandler(
-	IUnitOfWork unitOfWork,
-	IMediaUpdateService mediaUpdateService
-) : IRequestHandler<UpdateCustomerCommand, Result>
-	{
-		public async ValueTask<Result> Handle(
-			UpdateCustomerCommand command,
-			CancellationToken cancellationToken
-		)
-		{
-			Account? customer = await unitOfWork
-				.DynamicReadOnlyRepository<Account>()
-				.FindByConditionAsync(
-					new GetAccountByIdSpecification(command.AccountId),
-					cancellationToken
-				);
-			if (customer == null)
-			{
-				return Result.Failure(
-					new NotFoundError(
-						"Account not found",
-						Messager.Create<Account>().Message(MessageType.Found).Negative().BuildMessage()
-					)
-				);
-			}
+    public class UpdateCustomerHandler(IUnitOfWork unitOfWork)
+        : IRequestHandler<UpdateCustomerCommand, Result>
+    {
+        public async ValueTask<Result> Handle(
+            UpdateCustomerCommand command,
+            CancellationToken cancellationToken
+        )
+        {
+            Account? customer = await unitOfWork
+                .DynamicReadOnlyRepository<Account>()
+                .FindByConditionAsync(
+                    new GetAccountByIdSpecification(command.AccountId),
+                    cancellationToken
+                );
+            if (customer == null)
+            {
+                return Result.Failure(
+                    new NotFoundError(
+                        "Account not found",
+                        Messager
+                            .Create<Account>()
+                            .Message(MessageType.Found)
+                            .Negative()
+                            .BuildMessage()
+                    )
+                );
+            }
 
-			string? oldAvatar = customer.AvtUrl;
+            customer.FromUpdateCustomer(command.Account!);
 
-			customer.FromUpdateCustomer(command.Account!);
+            // update default claim
 
-			customer.AvtUrl = command.Account!.AvtUrl;
-			// update default claim
+            try
+            {
+                DbTransaction transaction = await unitOfWork.BeginTransactionAsync(
+                    cancellationToken
+                );
 
-			try
-			{
-				DbTransaction transaction = await unitOfWork.BeginTransactionAsync(cancellationToken);
+                await unitOfWork.Repository<Account>().UpdateAsync(customer);
+                await unitOfWork.SaveAsync(cancellationToken);
 
-				await unitOfWork.Repository<Account>().UpdateAsync(customer);
-				await unitOfWork.SaveAsync(cancellationToken);
+                await unitOfWork.CommitAsync(cancellationToken);
 
-				await unitOfWork.CommitAsync(cancellationToken);
-
-				await mediaUpdateService.DeleteMediaAsync(oldAvatar);
-				return Result.Success();
-			}
-			catch (Exception)
-			{
-				await mediaUpdateService.DeleteMediaAsync(customer.AvtUrl);
-				await unitOfWork.RollbackAsync(cancellationToken);
-				throw;
-			}
-		}
-	}
+                return Result.Success();
+            }
+            catch (Exception)
+            {
+                await unitOfWork.RollbackAsync(cancellationToken);
+                throw;
+            }
+        }
+    }
 }
--- src/AuthService/Application/Features/Customers/Command/Update/UpdateCustomerMapping.cs
+++ src/AuthService/Application/Features/Customers/Command/Update/UpdateCustomerMapping.cs
@@ -1,23 +1,19 @@
-﻿using Application.Features.Accounts.Commands.Update;
-using Domain.Aggregates.Accounts;
+﻿using Domain.Aggregates.Accounts;
 using Infrastructure.Constants;
 
 namespace Application.Features.Customers.Command.Update;
 
 public static class UpdateCustomerMapping
 {
-	public static Account FromUpdateCustomer(this Account customer, UpdateCustomerModel update)
-	{
-		customer.Update(
-			update.DisplayName,
-			update.Email,
-			update.PhoneNumber,
-			update.BirthDay != null ? DateOnly.FromDateTime((DateTime)update.BirthDay) : null,
-			status: update.Status,
-			role: ROLE.CUSTOMER,
-			gender: update.Gender
-		);
-		customer.AvtUrl = update.AvtUrl;
-		return customer;
-	}
+    public static Account FromUpdateCustomer(this Account customer, UpdateCustomerModel update)
+    {
+        customer.Update(
+            update.DisplayName,
+            update.PhoneNumber,
+            status: update.Status,
+            role: ROLE.CUSTOMER,
+            gender: update.Gender
+        );
+        return customer;
+    }
 }
