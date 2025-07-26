d25e41fec7532ed5199f4105518c34c04ea5c1fa
John Vo <jvanime212@gmail.com>
2025-07-26 20:33:05 +0700

    add point order

---
+++ src/EcommeceService/Infrastructure/Data/Migrations/20250726133255_20250726203235_Ecommerce_Migration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250726203235_Ecommerce_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "point",
+                table: "order",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "point",
+                table: "order");
+        }
+    }
+}
