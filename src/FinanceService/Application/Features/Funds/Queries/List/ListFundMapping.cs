cc20c8dc90440fb6a459ef42c2f01fd09650c475
John Vo <jvanime212@gmail.com>
2025-07-27 16:06:25 +0700

    fix list fund return

--- src/FinanceService/Application/Features/Funds/Queries/List/ListFundMapping.cs
+++ src/FinanceService/Application/Features/Funds/Queries/List/ListFundMapping.cs
@@ -27,7 +27,7 @@ public static class ListFundMapping
             Metadata = fund.Metadata,
             Status = fund.Status,
             Code = fund.Code,
-            User = fund.User.ToUserDTOResponse(),
+            User = fund.User != null ? fund.User.ToUserDTOResponse() : null,
             FundBehavior = fund.FundBehavior.ToFundBehaviorProjection(),
         };
     }
