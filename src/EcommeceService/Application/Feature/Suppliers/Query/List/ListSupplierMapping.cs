ec70d96816cffa227ab505c549dc1df1cec3e1da
John Vo <jvanime212@gmail.com>
2025-07-28 23:14:36 +0700

    fix status total inventory supplier

--- src/EcommeceService/Application/Feature/Suppliers/Query/List/ListSupplierMapping.cs
+++ src/EcommeceService/Application/Feature/Suppliers/Query/List/ListSupplierMapping.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Domain.Aggregates.Inventories.Enums;
 using Domain.Aggregates.Suppliers;
 
 namespace Application.Feature.Suppliers.Query.List
@@ -22,7 +23,11 @@ namespace Application.Feature.Suppliers.Query.List
                 Phone = supplier.Phone,
                 Description = supplier.Description,
                 Status = supplier.Status,
-                TotalInventory = supplier.ProductSupplyings.Sum(ps => ps.Quantity * ps.Price),
+                TotalInventory = supplier
+                    .ProductSupplyings.Where(x =>
+                        x.InventoryDocument.Status == InventoryStatus.Completed
+                    )
+                    .Sum(ps => ps.Quantity * ps.Price),
             };
     }
 }
