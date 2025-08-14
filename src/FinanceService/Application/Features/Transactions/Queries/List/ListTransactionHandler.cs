3be3eca29aec50a5aea57508f870f9f5317082c5
John Vo <jvanime212@gmail.com>
2025-08-14 18:19:47 +0700

    fix transaction list by customer

--- src/FinanceService/Application/Features/Transactions/Queries/List/ListTransactionHandler.cs
+++ src/FinanceService/Application/Features/Transactions/Queries/List/ListTransactionHandler.cs
@@ -1,3 +1,4 @@
+using Application.Common.Interfaces.Services;
 using Application.Common.Interfaces.UnitOfWorks;
 using Contracts.ApiWrapper;
 using Contracts.Common.QueryStringProcessing;
@@ -8,7 +9,7 @@ using Mediator;
 
 namespace Application.Features.Transactions.Queries.List
 {
-    public class ListTransactionHandler(IUnitOfWork unitOfWork)
+    public class ListTransactionHandler(IUnitOfWork unitOfWork, ICurrentAccount currentAccount)
         : IRequestHandler<ListTransactionQuery, Result<PaginationResponse<ListTransactionResponse>>>
     {
         public async ValueTask<Result<PaginationResponse<ListTransactionResponse>>> Handle(
@@ -22,11 +23,19 @@ namespace Application.Features.Transactions.Queries.List
             {
                 return validation;
             }
+            var role = currentAccount.Session!.Role;
+            long? customer = null;
+
+            if (role == "CUSTOMER")
+            {
+                customer = currentAccount.Id;
+            }
+
             return Result<PaginationResponse<ListTransactionResponse>>.Success(
                 await unitOfWork
                     .DynamicReadOnlyRepository<Transaction>()
                     .PagedListAsync(
-                        new ListTransactionSpecification(),
+                        new ListTransactionSpecification(customer),
                         request,
                         ListTransactionMapping.Selector(),
                         cancellationToken
--- src/FinanceService/Domain/Aggregates/Funds/Specifications/ListTransactionSpecification.cs
+++ src/FinanceService/Domain/Aggregates/Funds/Specifications/ListTransactionSpecification.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+using Shared.Kernel.Extentions;
 using Specification;
 using Specification.Builders;
 
@@ -5,9 +7,19 @@ namespace Domain.Aggregates.Funds.Specifications
 {
     public class ListTransactionSpecification : Specification<Transaction>
     {
-        public ListTransactionSpecification()
+        public ListTransactionSpecification(long? customerId = null)
         {
-            Query.OrderByDescending(x => x.TransactionAt).AsNoTracking().AsSplitQuery();
+            Expression<Func<Transaction, bool>> criteria = x => true;
+
+            if (customerId.HasValue)
+            {
+                criteria = criteria.And(x => x.CustomerId == customerId.Value);
+            }
+            Query
+                .Where(criteria)
+                .OrderByDescending(x => x.TransactionAt)
+                .AsNoTracking()
+                .AsSplitQuery();
         }
     }
 }
